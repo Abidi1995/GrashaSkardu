5acf0e5c9e4ab52029e30a66389ee03d333bdbca
hussainhamid1995@outlook.com <hussainhamid1995@outlook.com>
2021-06-17 14:21:31 +0500

    Add project files.

---
+++ GrashaSkardu/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrashaSkardu.Models
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context>dbContextOptions):base(dbContextOptions){
+
+    }
+}
---
+++ GrashaSkardu/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrashaSkardu.Models
+{
+    public class Student
+    {
+        [Key]
+        
+        public int StudentId { get; set; }
+        [Required(ErrorMessage ="input your complete name")]
+        public string StudentName { get; set; }
+        [Required(ErrorMessage ="input your father name")]
+        public string StudentFname { get; set; }
+        [Required(ErrorMessage ="please enter your class")]
+        public string StudentClass { get; set; }
+        [Required(ErrorMessage ="enter your program")]
+        public string StudentPrgram { get; set; }
+        [Required(ErrorMessage ="enter your section")]
+        public string StudentSection { get; set; }
+    }
+}
